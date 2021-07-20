8140c15d4e02cfb163a3c2e9dc32ce8b806178dd
5cr1pt <codejarry@gmail.com>
2021-07-20 14:18:32 +0800

    update: build.sh&docker-compose.yml; fix mysql conflict

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 cd ./JIRA
 docker build -t aladdinding/jira .
 cd ..
-cd Confluence
+cd ./Confluence
 docker build -t aladdinding/confluence .
 cd ..
-cd MYSQL
+cd ./MYSQL
 docker build -t aladdinding/mysql:5.7 .
 docker-compose up -d
