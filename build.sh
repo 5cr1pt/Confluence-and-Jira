4d838040d8a82b97920cc4b91316f6d855f3a3c2
5cr1pt <codejarry@gmail.com>
2021-07-20 14:12:36 +0800

    add: build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd ./JIRA
+docker build -t aladdinding/jira .
+cd ..
+cd Confluence
+docker build -t aladdinding/confluence .
+cd ..
+cd MYSQL
+docker build -t aladdinding/mysql:5.7 .
+docker-compose up -d
